b91e9f3b1b9682f7999e336d984eb48acb02f1a7
Mickae Truman <mtruman@nyit.edu>
2019-02-21 04:01:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
-students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-def my_collect(students)
-my_collect(students) do |name|
-  students.split(" ").first
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+  new_collection << yield(collection[i])
+  i += 1 
 end
-end
-
-def my_collect(languages)
-  my_collect(collection) do |lang|
-    lang.upcase
+   new_collection
   end
